e5571f0d4d3f8a7d8c677007468e08646dd0a951
IJMadalenA <developmentim@qaleon.com>
2023-05-19 11:48:22 +0200

    docker system.

---
+++ core_flow/initializer.sql
@@ -0,0 +1,8 @@
+-- ####################### --
+-- POSTGRESQL INITIALIZER. --
+-- ####################### --
+
+-- INSTALL EXTENSIONS.
+-- https://www.postgresql.org/docs/current/contrib-dblink-function.html
+CREATE EXTENSION dblink;
+
